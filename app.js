31a9931d620981a89298ea43017d3181d82d3ed8
ken3527549 <ken3527549@hotmail.com>
2017-11-07 16:17:07 +0000

    add pin

--- app.js
+++ app.js
@@ -7,18 +7,23 @@ var bot = linebot({
   channelSecret: '83088b5bcadb9ee57cf6c334bc380e89',
   channelAccessToken: 'EFibAncx5/27LpVFzz3/k/n1sEsoaQeVjqV2f8DDyLOydC3E0YpuPBcZFn5962mJT3pYe7dz4Tww5ouFBFA5Uia1inEXm5U0bfIJ+APu1ZiSWSNg5+ND5R68B/dnextLgSW9rkSHo916ADbuCOYqWAdB04t89/1O/w1cDnyilFU='
 });
+
+var adj = ['火爆', '正', '可愛', '帥氣', '機車', '漂亮', '有趣', '無聊', '色情'];
+var num = adj.length;
+
+var inAdj = adj[Math.floor((Math.random() * num) + 1)];
 bot.on('message', function(event) {
   console.log(event); //把收到訊息的 event 印出來看看
   if (event.message.type === 'text') {
   	var text = event.message.text;
   	switch (text) {
   		case '品翰':
-  			event.reply(`Thank you for sending ${text}. Your message is very important to us. The problem was caused by ${excuse.get()}`).then(function (data) {
+  			event.reply(`${text}是群組中最${inAdj}的人`).then(function (data) {
 			    console.log(data);
 			}).catch(function (error) {
 	   		 	console.log(error);
 			});
-  			break;
+		break;
 
   	}
   }
