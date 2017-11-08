d6e657525e2fb6af5f1ce6d23b0b61da4cf5e81f
ken3527549 <ken3527548@gmail.com>
2017-11-08 22:47:29 +0800

    add all cmds

--- cmds.js
+++ cmds.js
@@ -1,5 +1,5 @@
 var highschool = ['品翰', '慶生', '暉暉', '子雞', '大嫂'];
-var others = ['正妹', '爽嗎', '感覺如何', '指令'];
+var others = ['覺得冷', '爽嗎', '感覺如何', '指令'];
 
 var all = highschool.concat(others).join(' ');
 module.exports = {
