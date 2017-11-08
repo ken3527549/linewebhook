ebec4f9208c75271678a0491b9fb6996dcec2b53
ken3527549 <ken3527548@gmail.com>
2017-11-08 22:14:33 +0800

    add girls

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ bot.on('message', function(event) {
 			event.reply({
 				type: 'image',
 				originalContentUrl: ingirls,
-				previewImageUrl: ingirls
+				previewImageUrl: 'https://imgur.com/a/zvWL5'
 			}).then(function (data) {
 				console.log(data);
 			}).catch(function (error) {
