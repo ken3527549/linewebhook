6d16a3665883c363c8fa780ac7e77d145a46dcd6
ken3527549 <ken3527548@gmail.com>
2018-04-14 15:49:46 +0800

    Junior pix

--- cmds.js
+++ cmds.js
@@ -1,11 +1,5 @@
 var highschool = ['品品', '慶生', '暉暉', '子雞', '大嫂'];
 var others = ['抽', '幹嘛', '哈哈', '指令'];
-var pixURL = {
-    '1':'https://i.imgur.com/1IZLpS2.jpg',
-    '2':'https://i.imgur.com/411zVIL.jpg',
-    '3':'https://i.imgur.com/Par86ZM.jpg',
-    '4':'https://i.imgur.com/MmJTfYk.jpg'
-};
 var pix = {};
 for (let index = 1; index < 44; index++) {
     // pix.push(index.toString());
@@ -17,6 +11,5 @@ module.exports = {
     highschool,
     others,
     all,
-    pix,
-    pixURL
+    pix
 }
--- statements.js
+++ statements.js
@@ -2,9 +2,7 @@ var adj = ['最火爆', '最正', '最可愛', '最帥氣', '最機車', '最漂
 '最無聊', '最色情', '最神奇', '最87', '最6', '最好笑', '最邊緣', '滷味最重', '最神'];
 
 var girls = ['https://i.imgur.com/uoY6QXR.jpg',
-    'https://cdn.stocksnap.io/img-thumbs/960w/NI5BLWYFUG.jpg',
-    'https://i.imgur.com/RKTOTQi.jpg',
-    'https://i.imgur.com/SbzXAKk.jpg'];
+    'https://cdn.stocksnap.io/img-thumbs/960w/NI5BLWYFUG.jpg'];
 
 module.exports = {
     adj,
