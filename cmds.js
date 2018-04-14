b02b6699e7211a35e8e5a0aa00bda9200bd517c2
ken3527549 <ken3527548@gmail.com>
2018-04-14 14:40:03 +0800

    test4

--- cmds.js
+++ cmds.js
@@ -1,10 +1,15 @@
 var highschool = ['品品', '慶生', '暉暉', '子雞', '大嫂'];
 var others = ['抽', '幹嘛', '哈哈', '指令'];
 var pix = [];
+for (let index = 1; index < 44; index++) {
+    const element = array[index];
+    pix.push(index);
+}
 
 var all = highschool.concat(others).join(' ');
 module.exports = {
     highschool,
     others,
-    all
+    all,
+    pix
 }
