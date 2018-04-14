fc4e09d7a9742d4b69dc856d3eb329e7d0a1ba1c
ken3527549 <ken3527548@gmail.com>
2018-04-14 14:32:23 +0800

    test4

--- statements.js
+++ statements.js
@@ -4,7 +4,8 @@ var adj = ['最火爆', '最正', '最可愛', '最帥氣', '最機車', '最漂
 var girls = ['https://i.imgur.com/uoY6QXR.jpg',
     'https://cdn.stocksnap.io/img-thumbs/960w/NI5BLWYFUG.jpg',
     'https://35.201.142.154/logo-design-26.jpg',
-    'https://i.imgur.com/RKTOTQi.jpg'];
+    'https://i.imgur.com/RKTOTQi.jpg',
+    'https://imgur.com/SbzXAKk'];
 
 module.exports = {
     adj,
