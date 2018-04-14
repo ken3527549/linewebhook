710d1c3117902bd1fb7f5b83f620e37c3f00459e
ken3527549 <ken3527548@gmail.com>
2018-04-14 14:31:20 +0800

    test4

--- statements.js
+++ statements.js
@@ -3,7 +3,8 @@ var adj = ['最火爆', '最正', '最可愛', '最帥氣', '最機車', '最漂
 
 var girls = ['https://i.imgur.com/uoY6QXR.jpg',
     'https://cdn.stocksnap.io/img-thumbs/960w/NI5BLWYFUG.jpg',
-    'https://35.201.142.154/logo-design-26.jpg'];
+    'https://35.201.142.154/logo-design-26.jpg',
+    'https://i.imgur.com/RKTOTQi.jpg'];
 
 module.exports = {
     adj,
